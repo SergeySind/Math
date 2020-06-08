288d6b3a1c8929c90814e9bda719225a28ab56dc
Sergey S <sbw@bk.ru>
2020-06-08 19:36:46 +0400

    Lab6: рефакторинг

--- source/Lab6.java
+++ source/Lab6.java
@@ -1,74 +1,57 @@
-//import java.util.function.DoubleFunction;
-//import java.util.function.DoubleUnaryOperator;
-//
-//public class Lab6 {
-//    private static final double EPSILON = 0.000001;
-//    private static final int ITERATION_COUNT = 100;
-//    private static final DoubleFunction<Double> function1 = x -> 2 * Math.atan(x) - 3 * x + 2;
-//    private static final DoubleFunction<Double> function2 = x -> 2 * Math.pow(x, 4) + 8 * Math.pow(x, 3) + 8 * Math.pow(x, 2) - 1;
-//    private static final DoubleFunction<Double> function3 = x -> Math.log(x + 2) / Math.log(2) * (x - 1) - 1;
-//    private static final DoubleFunction<Double> function4 = x -> Math.sin(x - 0.5) - x + 0.8;
-//
-//    //    N13
-////    public static final DoubleUnaryOperator function1 = x -> Math.pow(3, x) + 2 * x - 5;
-////    public static final DoubleUnaryOperator function2 = x -> x * x * x * x - 4 * x * x * x - 8 * x * x + 1;
-////    public static final DoubleUnaryOperator function3 = x -> x * x - 3 + Math.pow(0.5, x);
-////    public static final DoubleUnaryOperator function4 = x -> (x - 2) * (x - 2) * Math.log10(x + 11) - 1;
-//
-////    N15
-////    public static final DoubleUnaryOperator function1 = x -> Math.pow(3, x - 1) - 4 - x;
-////    public static final DoubleUnaryOperator function2 = x -> 2 * x * x * x - 9 * x * x - 60 * x + 1;
-////    public static final DoubleUnaryOperator function3 = x -> (x - 3) * (x - 3) * lohHalf(x - 2) + 1;
-////    public static final DoubleUnaryOperator function4 = x -> 5 * Math.sin(x) - x + 1;
-////    public static double lohHalf(double n) {
-////        return Math.log(n) / Math.log(0.5);
-////    }
-//
-//    public static void main(String[] args) {
-//        double[] Xs1 = {-0.577350269189626, 0.577350269189626};
-//        double[] Xs2 = {0, -1, -2};
-//        double[] Xs3 = {-1, 1};
-//        double[] Xs4 = {0.5, 6.783};
-//
-//
-//        System.out.println("2arctg(x)-3x+2");
-//        ext(Xs1, function1, -1);
-//        Xs1[0] = 0.914;
-//        ext(Xs1, function1, +1);
-//
-//        System.out.println("2x^4+8x^3+8x^2-1");
-//        ext(Xs2, function2, -1);
-//        Xs2[0] = 0;
-//        ext(Xs2, function2, +1);
-//        System.out.println(
-//                "Определим промежутки по графикам функций\n" +
-//                        "[log2(x+2)](x-1)\n" +
-//                        bisectionPrototype(function3, -1.5, 0, EPSILON) + "\n" +
-//                        bisectionPrototype(function3, 0, 1.5, EPSILON) + "\n" +
-//                        "sin(x-0.5)-x+0.8\n" + bisectionPrototype(function3, -3, -2, EPSILON) + "\n" +
-//                        bisectionPrototype(function4, -1, 1, EPSILON) + "\n" +
-//                        bisectionPrototype(function4, 2, 3, EPSILON));
-//    }
-//
-//    private static void ext(double[] xs1, DoubleUnaryOperator function1, int mod) {
-//        for (int i = 0; i < ITERATION_COUNT; i++) {
-//            if (function1.applyAsDouble(xs1[0]) > 0) {
-//                System.out.println(bisectionPrototype(function1, xs1[0] - 1, xs1[0], EPSILON));
-//                break;
-//            } else if (function1.applyAsDouble(xs1[0]) < function1.applyAsDouble(xs1[0] + mod)) {
-//                xs1[0] = xs1[0] + mod;
-//            }
-//        }
-//    }
-//
-//    private static double bisectionPrototype(DoubleUnaryOperator function, double lowerBound, double upperBound, double precision) {
-//        double media = 0;
-//        while ((upperBound - lowerBound) > precision) {
-//            media = (lowerBound + upperBound) / 2;
-//            if (function.applyAsDouble(media) == 0) break;
-//            if (function.applyAsDouble(media) * function.applyAsDouble(lowerBound) < 0) upperBound = media;
-//            else lowerBound = media;
-//        }
-//        return media;
-//    }
-//}
+import java.util.function.DoubleUnaryOperator;
+
+public class Lab6 {
+    private static final double EPSILON = 0.001;
+    private static final String equantion1 = "3^(x-1)-4-x=0";
+    private static final String equantion2 = "2*x^3-9*x^2-60*x+1=0";
+    private static final String equantion3 = "(x-3)^2*log0.5(x-5)= -1";
+    private static final String equantion4 = "5*sin(x)=x-1";
+
+    private static final DoubleUnaryOperator function1 = x -> Math.pow(3, x - 1) - 4 - x;
+    private static final DoubleUnaryOperator function2 = x -> 2 * x * x * x - 9 * x * x - 60 * x + 1;
+    private static final DoubleUnaryOperator function3 = x -> (x - 3) * (x - 3) * logHalf(x - 2) + 1;
+    private static final DoubleUnaryOperator function4 = x -> 5 * Math.sin(x) - x + 1;
+
+    public static double logHalf(double n) {
+        return Math.log(n) / Math.log(0.5);
+    }
+
+    public static void main(String[] args) {
+        double[][] equation1_roots_range = {{-5, -3}, {2, 3}};
+        double[][] equation2_roots_range = {{-4, -3}, {0, 1}, {8, 9}};
+        double[][] equation3_roots_range = {{}, {}}; //todo
+        double[][] equation4_roots_range = {{-.8*Math.PI, -.6*Math.PI}, {-.2*Math.PI, 0},{.8*Math.PI, Math.PI}};
+        System.out.printf("Calculating roots for equation %s:\n", equantion1);
+        for (int i = 0; i < equation1_roots_range.length; i++) {
+            System.out.printf("x%d = %.4f\n",i+1, bisectionPrototype(function1, equation1_roots_range[i], EPSILON));
+        }
+        System.out.printf("Calculating roots for equation %s:\n", equantion2);
+        for (int i = 0; i < equation2_roots_range.length; i++) {
+            System.out.printf("x%d = %.4f\n",i+1, bisectionPrototype(function2, equation2_roots_range[i], EPSILON));
+        }
+        System.out.printf("Calculating roots for equation %s:\n", equantion3);
+        for (int i = 0; i < equation3_roots_range.length; i++) {
+            System.out.printf("x%d = %.4f\n",i+1, bisectionPrototype(function3, equation3_roots_range[i], EPSILON));
+        }
+        System.out.printf("Calculating roots for equation %s:\n", equantion4);
+        for (int i = 0; i < equation4_roots_range.length; i++) {
+            System.out.printf("x%d = %.4f\n",i+1, bisectionPrototype(function4, equation4_roots_range[i], EPSILON));
+        }
+    }
+
+    private static double bisectionPrototype(DoubleUnaryOperator function, final double[] bounds, double precision) {
+        if (bounds.length != 2) throw new ArrayIndexOutOfBoundsException();
+        double upperBound = bounds[1];
+        double lowerBound = bounds[0];
+        double media = 0;
+        while ((upperBound - lowerBound) > precision) {
+            media = (lowerBound + upperBound) / 2;
+            if (function.applyAsDouble(media) == 0) break;
+            if (function.applyAsDouble(media) * function.applyAsDouble(lowerBound) < 0) upperBound = media;
+            else lowerBound = media;
+        }
+        return media;
+    }
+
+    private static void rootsPrinting(){}
+}
